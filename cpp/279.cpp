190d980a364b18afa8859abfd5ef708f54cfa0b0
i-am-harveyt <haotingtong30@gmail.com>
2022-11-23 15:30:09 +0800

    279, add iterative method, perform better.

--- cpp/279.cpp
+++ cpp/279.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <unordered_map>
+#include <vector>
 
 using namespace std;
 
@@ -7,24 +8,43 @@ using namespace std;
  * This code can run, but performance is bad,
  * I think I still need some math analysis skill
  */
-class Solution {
+class SolutionOld {
 public:
-  int dp(int n, unordered_map<int, int> mp) {
+  int dp(int n, unordered_map<int, int> &mp) {
     if (n == 0)
       return 0;
     if (mp.find(n) != mp.end())
       return mp[n];
 
     mp[n] = n;
-    for (int i = 1; i * i < n; i++) {
+    for (int i = 1; i * i <= n; i++) {
       int val = dp(n - i * i, mp);
       mp[n] = (mp[n] < val) ? mp[n] : val;
     }
-    return mp[n] + 1;
+    ++mp[n];
+    return mp[n];
   }
 
   int numSquares(int n) {
     unordered_map<int, int> mp;
-    return dp(n, mp);
+    dp(n, mp);
+    return mp[n];
+  }
+};
+
+// interative method
+class Solution {
+public:
+  int numSquares(int n) {
+    vector<int> arr(n + 1);
+    for (int i = 0; i < arr.size(); i++)
+      arr[i] = i;
+
+    for (int i = 0; i <= n; i++)
+      for (int j = 1; j * j <= i; j++)
+        if (arr[i - j * j] + 1 < arr[i])
+          arr[i] = arr[i - j * j] + 1;
+
+    return arr[n];
   }
 };
--- go/279.go
+++ go/279.go
@@ -1,9 +1,10 @@
 package main
 
-/* 
+/*
  * This code can run but not work well
  * I still need some math analysis
  */
+/* Recursive
 func dp(n int, m map[int]int) int {
 	if n == 0 {
 		return 0
@@ -25,4 +26,23 @@ func dp(n int, m map[int]int) int {
 
 func numSquares(n int) int {
 	return dp(n, make(map[int]int))
+}
+*/
+
+/* Iterative */
+func numSquares(n int) int {
+	dp := make([]int, n+1)
+	for i := 0; i <= n; i++ {
+		dp[i] = i
+	}
+
+	for i := 1; i <= n; i++ {
+		for j := 1; j*j <= i; j++ {
+			if dp[i-j*j]+1 < dp[i] {
+				dp[i] = dp[i-j*j] + 1
+			}
+		}
+	}
+
+	return dp[n]
 }
